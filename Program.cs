70da8c2ee7e7a7d73b61f101afff9b8b5c13d331
CHRsource <iko_1864@icloud.com>
2023-05-23 13:54:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Vector a = new();
+Console.WriteLine($"Введите вектор из {a.Arr.Length} вещественных чисел:");
+a.InputVec();
+Console.WriteLine("Массив: " + a);
+
+a = new(10);
+Console.WriteLine("Массив: " + a);
+a.InputVec();
+Console.WriteLine("Массив: " + a);
+Console.WriteLine(a.MaxElem());
+Console.WriteLine(a.IndMinElem());
+
+Vector b = a.VecPositiveElem();
+Console.WriteLine("Массив: " + b);
+
+a = new(new double[] { 1, 2.0, 3.5, -5.1 });
+Console.WriteLine("Массив: " + a);
+Console.WriteLine(a.Module());
+
+Vector first = new(new double[] { 1, 8, 43 });
+//Vector second = new(new double[] { 9, -45.4, 0 });
+Vector second = new(new double[] { 1, 8, 43 });
+Vector? sum = Vector.Sum(first, second);
+if (sum == null)
+    Console.WriteLine("Векторы разной размерности. Суммы не существует.");
+else Console.WriteLine("Массив из суммы: " + sum);
+
+double? scal_prod = Vector.ScalarProduct(first, second);
+if (scal_prod == null) Console.WriteLine("Векторы разной размерности. Скалярного произведения не существует.");
+else Console.WriteLine($"Скалярное произведение векторов = {scal_prod}");
+
+if (Vector.CheckEquality(first, second)) { Console.WriteLine("Векторы равны"); }
+else Console.WriteLine("Векторы не равны");
---
+++ Vector.cs
@@ -0,0 +1,74 @@
+﻿#pragma warning disable
+public class Vector
+{
+    public double[] Arr { get; set; }
+    public Vector(double[] a) { Arr = a; }
+    public Vector() : this(1)
+    { }
+    public Vector(int n) {
+        if (n <= 0) { n = 1; }
+        Arr = new double[n];
+    }
+
+    public void InputVec() { 
+        for (int i = 0; i < Arr.Length; i++) { Arr[i] = Convert.ToDouble(Console.ReadLine()); }
+    }
+    public override string ToString()
+    {
+        string array = "";
+        for (int i = 0; i < Arr.Length; i++) { array = array + Arr[i] + " "; }
+        return array;
+    }
+    public double Module()
+    {
+        double sum_square = 0;
+        for (int i = 0; i < Arr.Length; i++) { sum_square += Math.Pow(Arr[i], 2); }
+        return Math.Round(Math.Sqrt(sum_square), 3);
+    }
+    public double MaxElem()
+    {
+        int ind = 0;
+        for (int i = 1; i < Arr.Length; i++) { if (Arr[i] > Arr[ind]) ind = i; }
+        return Arr[ind];
+    }
+    public int IndMinElem()
+    {
+        int ind = 0;
+        for (int i = 1; i < Arr.Length; i++) { if (Arr[i] < Arr[ind]) ind = i; }
+        return ind;
+    }
+    public Vector VecPositiveElem()
+    {
+        int size = 0, count = 0;
+        for (int i = 0; i < Arr.Length; i++) { if (Arr[i] > 0) size++; }
+        Vector arr_new = new(size);
+        for (int i = 0; i < Arr.Length; i++) { if (Arr[i] > 0) arr_new.Arr[count++] = Arr[i]; }
+        return arr_new;
+    }
+    public static Vector? Sum(Vector a, Vector b)
+    {
+        if (a.Arr.Length != b.Arr.Length) {
+            return null;
+        }
+        Vector res = new(a.Arr.Length);
+        for (int i = 0; i < a.Arr.Length; i++) { res.Arr[i] = a.Arr[i] + b.Arr[i]; }
+        return res;
+    }
+    public static double? ScalarProduct(Vector a, Vector b)
+    {
+        if (a.Arr.Length != b.Arr.Length) {
+            return null;
+        }
+        double res = 0;
+        for (int i = 0; i < a.Arr.Length; i++) { res += a.Arr[i] * b.Arr[i]; }
+        return res;
+    }
+    public static bool CheckEquality(Vector a, Vector b)
+    {
+        if (a.Arr.Length != b.Arr.Length) return false;
+        for (int i = 0; i < a.Arr.Length; i++) {
+            if (a.Arr[i] != b.Arr[i]) return false;
+        }
+        return true;
+    }
+}
